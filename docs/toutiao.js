fad5b50c5e3a0dd1eefdcf49de8e21dbc5b1e7e7
hirgb <hirgb.com@gmail.com>
2019-04-25 10:28:34 +0800

    toutiao.js

--- docs/toutiao.js
+++ docs/toutiao.js
@@ -1,7 +1,6 @@
 (function(){
     let e = document.createElement('div')
-    e.style.height = '80px'
-    e.style.backgroundColor = '#00000020'
+    e.style.height = '100px'
 
     let btn = document.createElement('span')
     btn.style.textAlign = 'center'
@@ -10,6 +9,9 @@
     btn.style.display = 'inline-block'
     btn.style.height = '80px'
     btn.style.borderRadius = '40px'
+    btn.style.fontSize = '40px'
+    btn.style.lineHeight = '80px'
+    btn.style.padding = '0 40px'
     btn.innerText = '三角兽'
 
     e.append(btn)
