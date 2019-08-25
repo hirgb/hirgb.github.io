a6d2fa0f710dff2e518e61795ac4c25c6b2901e5
hirgb <hirgb.com@gmail.com>
2019-08-25 15:23:42 +0800

    update

--- sw.js
+++ sw.js
@@ -1,25 +1,54 @@
 const version = '1.0'
 const cacheName = 'hirgb-cache'
 
-this.addEventListener('install', (e)=>{
-    // console.log('install success');
-})
-
-this.addEventListener('activate', (e)=>{
-    // console.log('activate');
-})
-
-this.addEventListener('fetch', (e)=>{
+this.addEventListener('fetch', (e) => {
+    let url = e.request.url
     e.respondWith(
-        caches.open(cacheName).then((cache)=>{
-            return cache.match(e.request.url)
-        }).then((response)=>{
-            if (response) {
-                // console.log(`cache: ${e.request.url}`);
-                return response
+        caches.open(cacheName)
+        .then((cache) => {
+            return cache.match(url)
+        })
+        .then(res => {
+            if (res) {
+                return res
+            } else {
+                let purl = parse(url)
+                if (purl.pathname && purl.pathname.startsWith('/img')) {
+                    caches.open(cacheName)
+                    .then(cache => {
+                        cache.add(url)
+                    })
+                    return fetch(url)
+                } else {
+                    return fetch(e.request)
+                }
             }
-            // console.log(`net: ${e.request.url}`);
-            return fetch(e.request)
         })
     )
 })
+
+function parse(url) {
+    let urlParseRE = /^\s*(((([^:\/#\?]+:)?(?:(\/\/)((?:(([^:@\/#\?]+)(?:\:([^:@\/#\?]+))?)@)?(([^:\/#\?\]\[]+|\[[^\/\]@#?]+\])(?:\:([0-9]+))?))?)?)?((\/?(?:[^\/\?#]+\/+)*)([^\?#]*)))?(\?[^#]+)?)(#.*)?/;
+
+    let matches = urlParseRE.exec(url || "") || [];
+
+    return {
+        href: matches[0] || "",
+        hrefNoHash: matches[1] || "",
+        hrefNoSearch: matches[2] || "",
+        domain: matches[3] || "",
+        protocol: matches[4] || "",
+        doubleSlash: matches[5] || "",
+        authority: matches[6] || "",
+        username: matches[8] || "",
+        password: matches[9] || "",
+        host: matches[10] || "",
+        hostname: matches[11] || "",
+        port: matches[12] || "",
+        pathname: matches[13] || "",
+        directory: matches[14] || "",
+        filename: matches[15] || "",
+        search: matches[16] || "",
+        hash: matches[17] || ""
+    };
+};
